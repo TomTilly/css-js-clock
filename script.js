daf150cfb20246635bd0ae7c41032f9c619c7db5
Tom Tilly <tomtillistrand@gmail.com>
2019-04-23 20:10:22 -0400

    add clock logic

--- script.js
+++ script.js
@@ -2,35 +2,35 @@ const hrHand = document.querySelector('.hr-hand');
 const minHand = document.querySelector('.min-hand');
 const secondsHand = document.querySelector('.seconds-hand');
 
-
-function initClock(){
-	const currentDate = new Date();
-	const currentHrs = currentDate.getHours();
-	const currentMin = currentDate.getMinutes();
-	const currentSec = currentDate.getSeconds();
-
-	hrHand.style.transform = `rotate(${degreesToRotate({ hr: currentHrs })}deg)`;
-	minHand.style.transform = `rotate(${degreesToRotate({ min: currentMin })}deg)`;
-	secondsHand.style.transform = `rotate(${degreesToRotate({ sec: currentSec })}deg)`;
-}
-
 function degreesToRotate(time){
-	if(time.hr){
-		return (time.hr * 30) - 90;
+	if('hrs' in time){
+		return (time.hrs * 30) - 90;
 	}
-	if(time.min){
+	if('min' in time){
 		return (time.min * 6) - 90;
 	}
-	if(time.sec){
+	if('sec' in time){
 		return (time.sec * 6) - 90;
 	}
 }
 
-function moveHands(){
-
+function getCurrentTime(){
+	const currentDate = new Date();
+	return {
+		hrs: currentDate.getHours(),
+		min: currentDate.getMinutes(),
+		sec: currentDate.getSeconds(),
+	}
 }
 
-initClock();
+function moveHands(){
+	const currentTime = getCurrentTime();
+	hrHand.style.transform = `rotate(${degreesToRotate({ hrs: currentTime.hrs })}deg)`;
+	minHand.style.transform = `rotate(${degreesToRotate({ min: currentTime.min })}deg)`;
+	secondsHand.style.transform = `rotate(${degreesToRotate({ sec: currentTime.sec })}deg)`;
+}
+
+moveHands();
 setInterval(moveHands, 1000);
 
 // Hr hand moves 30 degrees every increment
