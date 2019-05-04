e7627500d365d9b2a1bac3c248340e21c746f11c
Tom Tilly <tomtillistrand@gmail.com>
2019-05-04 16:21:35 -0400

    remove transition animation when going from 59 seconds to 0 in order to avoid backwards rotation

--- script.js
+++ script.js
@@ -6,8 +6,17 @@ function setTime() {
     const now = new Date();
 
     const seconds = now.getSeconds();
-	const secondsDegrees = ((seconds / 60) * 360) - 90;
+	let secondsDegrees = ((seconds / 60) * 360) - 90;
     secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if(seconds === 59){
+        secondsDegrees = -96;
+        secondsHand.classList.add('no-transition');
+        secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        setTimeout( function () {
+            secondsHand.classList.remove('no-transition')
+        }, 1000);
+    }
+    console.log(secondsDegrees);
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) - 90;
